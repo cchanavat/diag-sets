24b5293a2fc4daf853565e20c7ed89ccba67a6ff
mchana <mathieu.chanavat@gmail.com>
2022-11-18 15:25:53 +0100

    first commit

---
+++ src/finite.lean
@@ -0,0 +1,255 @@
+import data.finset.basic
+import order.cover
+import order.category.FinPartialOrder
+
+/--
+  This file provide some useful lemmas about the finiteness property of P
+  In particular
+  `no_infinite_increasing_seq` and `no_infinite_decreasing_seq` prove that there are 
+  no infinite increasing/decreasing sequences in P
+  and `no_forall_exists_lt` and `no_forall_exists_gt` prove that there are no statement of
+  the form ∀ x ∈ U, ∃ y ∈ U, x < (or >) y, with `U : finset P` 
+-/
+
+variables (P : FinPartialOrder) [decidable_eq P] [@decidable_rel P (<)]
+
+
+instance decidable_lt_inv : @decidable_rel P (>) := by apply_instance
+instance decidable_eq_dual : decidable_eq Pᵒᵈ := by apply_instance
+instance decidable_rel_dual : @decidable_rel (Pᵒᵈ) (<) := by apply_instance
+instance decidable_le : @decidable_rel P (≤) := 
+begin
+  intros x y,
+  rw le_iff_eq_or_lt,
+  apply or.decidable,
+end
+
+
+
+-- Some useful lemma about finiteness
+namespace finite
+
+variable {P}
+
+abbreviation points : finset P := fintype.elems P
+
+def greater_or_eq (x : P) : finset P := finset.filter (λ y : P, x ≤ y) points
+def greater (x : P) : finset P := finset.filter (λ y : P, x < y) points
+
+lemma greater_iff_in_greater (x y : P) : y ∈ greater x ↔ x < y := 
+begin
+  erw finset.mem_filter, simp [fintype.complete]
+end  
+
+-- We prove that there could not be any strictly increasing sequence
+lemma monotonic_of_increasing {s : ℕ → P} (hinc : ∀ n, s n < s (n + 1)) :
+  ∀ i j, i < j → s i < s j :=
+begin
+  intros i j, revert i,
+  induction j with j hj,
+  simp,
+  intros i hi,
+  by_cases i = j,
+  rw h, apply hinc,
+  have i_lt_j : i < j := array.push_back_idx hi h,
+  specialize hj i i_lt_j,
+  apply lt_trans hj,
+  apply hinc
+end
+
+lemma injective_of_increasing {s : ℕ → P} (hinc : ∀ n, s n < s (n + 1)) : function.injective s :=
+begin
+  intros x y h,
+  wlog h : x ≤ y,
+  rw le_iff_lt_or_eq at h,
+  cases h,
+  { exfalso, 
+    have lt := monotonic_of_increasing hinc x y h,
+    simp at lt, 
+    exact ne_of_lt lt h_1 },
+  { exact h }
+end
+
+lemma no_infinite_increasing_seq (s : ℕ → P) : ¬ (∀ n, s n < s (n + 1)) :=
+begin
+  intro hinc,
+  have i : function.injective s := injective_of_increasing hinc,
+  have inf_P : infinite P := infinite.of_injective s i,
+  rw ←not_finite_iff_infinite at inf_P,
+  exact inf_P (finite.of_fintype P)
+end
+
+lemma injective_of_neq_preds {s : ℕ → P} (hinc : ∀ n m, n < m → s n ≠ s m) : function.injective s :=
+begin
+  intros n m,
+  wlog h : n ≤ m,
+  rw le_iff_lt_or_eq at h,
+  cases h,
+  { intro hs, exfalso, exact hinc _ _ h hs },
+  { rw h, intro, refl }
+end
+
+-- No sequence in P where s_m is different from s_n, n < m  
+lemma no_forall_neq_preds (s : ℕ → P) : ¬ (∀ n m, n < m → s n ≠ s m) :=
+begin
+  intros hneq,
+  have i : function.injective s := injective_of_neq_preds hneq,
+  have inf_P : infinite P := infinite.of_injective s i,
+  rw ←not_finite_iff_infinite at inf_P,
+  exact inf_P (finite.of_fintype P)
+end
+
+variables {U : finset P} {x0 : P} (hx0 : x0 ∈ U) 
+
+def seq_A_E' (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) : ℕ → { x : P // x ∈ U } 
+| 0       := ⟨x0, hx0⟩
+| (n + 1) := ⟨(h (seq_A_E' n).val (seq_A_E' n).prop).val, 
+              (h (seq_A_E' n).val (seq_A_E' n).prop).prop.left⟩ 
+
+def seq_A_E (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) : ℕ → P := λ n, (seq_A_E' hx0 h n).val
+def seq_A_E_in (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) (n : ℕ)  : 
+  (seq_A_E hx0 h n) ∈ U := (seq_A_E' hx0 h n).prop
+
+lemma seq_A_E_increasing (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) : 
+  ∀ n, seq_A_E hx0 h n < seq_A_E hx0 h n.succ :=
+begin
+  intro n,
+  have g := (h (seq_A_E hx0 h n) (seq_A_E_in hx0 h n)).prop.right,
+  convert g
+end
+
+lemma no_infinite_decreasing_seq (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) (s : ℕ → P) : 
+  ¬ (∀ n, s (n + 1) < s n) :=
+begin
+  intro h,
+  haveI := decidable_rel_dual P,
+  apply @no_infinite_increasing_seq (FinPartialOrder.dual.obj P) _ _ s h,
+end
+
+-- Therefore, there cannot be true statement of the form (∀ x : U, { y : U // x < y })
+-- which is a quite useful argument(?)
+lemma no_forall_exists_lt (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x < y }) : false :=
+no_infinite_increasing_seq (seq_A_E hx0 h) (seq_A_E_increasing _ _)
+
+lemma no_forall_exists_gt (hx0 : x0 ∈ U) (h : ∀ x ∈ U, { y : P // y ∈ U ∧ y < x }) : false :=
+begin
+  haveI := decidable_rel_dual P,
+  apply @no_infinite_increasing_seq (FinPartialOrder.dual.obj P) _ _ _ (seq_A_E_increasing hx0 h)
+end
+
+-- with the axiom of choice
+lemma no_forall_exists_lt' (h : ∀ x ∈ U, ∃ y ∈ U, x < y) : false :=
+no_forall_exists_lt hx0
+begin
+  intros x hx,
+  have k := classical.indefinite_description _ (h x hx),
+  simp at k,
+  exact k  
+end
+
+lemma no_forall_exists_gt' (h : ∀ x ∈ U, ∃ y ∈ U, y < x) : false :=
+no_forall_exists_gt hx0
+begin
+  intros x hx,
+  have k := classical.indefinite_description _ (h x hx),
+  simp at k,
+  exact k  
+end
+
+-- A variation with ≠ in place of < (should have done that first...)
+def seq_A_E_neq' (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x ≠ y }) : ℕ → { x : P // x ∈ U } 
+| 0       := ⟨x0, hx0⟩
+| (n + 1) := ⟨(h (seq_A_E_neq' n).val (seq_A_E_neq' n).prop).val, 
+              (h (seq_A_E_neq' n).val (seq_A_E_neq' n).prop).prop.left⟩ 
+
+def seq_A_E_neq (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x ≠ y }) : ℕ → P := λ n, (seq_A_E_neq' hx0 h n).val
+def seq_A_E_neq_in (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x ≠ y }) (n : ℕ)  : 
+  (seq_A_E_neq hx0 h n) ∈ U := (seq_A_E_neq' hx0 h n).prop
+
+lemma seq_A_E_neq_succ (h : ∀ x ∈ U, { y : P // y ∈ U ∧ x ≠ y }) : 
+  ∀ n, seq_A_E_neq hx0 h n ≠ seq_A_E_neq hx0 h n.succ :=
+begin
+  intro n,
+  have g := (h (seq_A_E_neq hx0 h n) (seq_A_E_neq_in hx0 h n)).prop.right,
+  convert g
+end
+
+lemma lt_iff_le_and_neq (x y : P) : x < y ↔ x ≤ y ∧ ¬x = y :=
+begin
+  rw lt_iff_le_not_le,
+  split; intro h; apply and.intro h.left; intro hneg,
+  { have h' : y ≤ x := by rw hneg, 
+    exact h.right h' },
+  { exact h.right (le_antisymm h.left hneg) }
+end
+
+def Ioc (x x' : P) := finset.filter (λ y, x < y ∧ y ≤ x') points
+lemma right_mem_Ioc (x x' : P) : x' ∈ Ioc x x' ↔ x < x' := 
+begin
+  unfold Ioc,
+  simp [fintype.complete]
+end
+
+lemma Ioc_lt_left {x x' y : P} (hin : y ∈ Ioc x x') : x < y := 
+begin
+  erw finset.mem_filter at hin,
+  simp [fintype.complete] at hin,
+  exact hin.left
+end
+
+lemma Ioc_le_right {x x' y : P} (hin : y ∈ Ioc x x') : y ≤ x' := 
+begin
+  erw finset.mem_filter at hin,
+  simp [fintype.complete] at hin,
+  exact hin.right
+end
+
+lemma Ioc_in_lt_lt {x x' y w : P} (hin : y ∈ Ioc x x') (hw : x < w ∧ w < y) : w ∈ Ioc x x':=
+begin
+  erw finset.mem_filter at *,
+  simp [fintype.complete] at *,
+  apply and.intro hw.left,
+  exact le_of_lt (lt_of_lt_of_le hw.right hin.right)
+end
+
+-- If x < x', then the sets of greater elements has a min ≤ x'
+lemma exists_min_greater {x x' : P} (hx' : x' ∈ greater x) : 
+  ∃ y ∈ Ioc x x', ∀ z ∈ Ioc x x', z ≤ y → z = y :=
+begin
+  by_contra' h,
+  have h' : ∀ (y ∈ Ioc x x'), ∃ (z ∈ Ioc x x'), z < y :=
+  begin
+    intros y hyin,
+    cases h y hyin with w h',
+    use w, 
+    rw lt_iff_le_and_neq w y,
+    exact h'
+  end,
+  have hIoc : x' ∈ Ioc x x' := 
+  begin
+    rw right_mem_Ioc,
+    rw greater_iff_in_greater at hx',
+    exact hx'
+  end,
+  apply no_forall_exists_gt' hIoc h'
+end
+
+-- If x < x', there is a covering element between x and x'
+lemma exists_cov_of_greater {x x' : P} (hx' : x' ∈ greater x) : ∃ y, x ⋖ y ∧ y ≤ x' := 
+begin
+  cases exists_min_greater hx',
+  cases h with w_in h,
+  use w,
+  rw greater_iff_in_greater at *,
+  split, split,
+  { exact Ioc_lt_left w_in },
+  { intros v hv hw, 
+    specialize h v,
+    specialize h (Ioc_in_lt_lt w_in (and.intro hv hw)) (le_of_lt hw),
+    rw h at hw,
+    apply lt_irrefl w,
+    exact hw },
+  { exact Ioc_le_right w_in }
+end
+
+end finite
---
+++ src/ogpos.lean
@@ -0,0 +1,669 @@
+import order.cover
+import order.category.FinPartialOrder
+import order.interval
+import order.locally_finite
+import combinatorics.quiver.basic
+import combinatorics.quiver.path
+import data.set.basic
+import data.nat.interval
+
+import finite
+
+
+
+-- 1.1.1 (Covering relation) ⋖ is already implemented in Lean, with tons of useful lemmas
+
+-- We require that our ogpos has a decidable equality and <
+variables (P : FinPartialOrder) [decidable_eq P] [@decidable_rel P (<)]
+
+
+namespace ogpos
+
+instance cover_decidable (x y : P) : decidable (x ⋖ y) := and.decidable
+
+-- 1.1.2 (Hasse diagram)
+instance hasse : quiver P := { hom := λ y x, x ⋖ y }
+
+variables {P} 
+
+abbreviation points : finset P := fintype.elems P
+abbreviation points_set : set P := fintype.elems P
+
+lemma incl_of_set (U : set P) : U ⊆ points_set := 
+begin
+  intros x _,
+  simp [fintype.complete]
+end
+
+instance (U : set P) : finite U :=
+finite.of_injective (set.inclusion (incl_of_set U)) (set.inclusion_injective _)
+
+noncomputable
+instance (U : set P) : fintype U := fintype.of_finite U
+
+noncomputable
+instance (U : set P) (x : P) : decidable (x ∈ U) := set.decidable_mem_of_fintype _ _
+
+-- noncomputable
+-- instance (U : set P) : decidable (U = ∅) := begin
+-- apply_instance
+-- end
+
+def hom_of_cov {x y : P} (hcov : y ⋖ x) : x ⟶ y := hcov
+def path_of_cov {x y : P} (hcov : y ⋖ x) : quiver.path x y := quiver.hom.to_path (hom_of_cov hcov)
+def cov_of_hom {x y : P} (f : x ⟶ y) : y ⋖ x := f
+
+lemma le_of_path {x y : P} (p : quiver.path y x) : x ≤ y :=
+begin
+  induction p with w z p h h1,
+  { refl },
+  { apply le_of_lt, 
+    apply has_lt.lt.trans_le (covby.lt (cov_of_hom h)) h1  }
+end
+
+lemma lt_of_path {x y : P} (hneq : x ≠ y) (p : quiver.path y x) : x < y :=
+begin 
+  rw lt_iff_le_and_ne,
+  exact and.intro (le_of_path p) hneq
+end
+
+lemma exists_cov_of_lt {x x' : P} (hle : x < x') : ∃ y, x ⋖ y ∧ y ≤ x' :=
+begin
+  rw ←finite.greater_iff_in_greater at hle,
+  exact finite.exists_cov_of_greater hle 
+end
+
+open set
+
+-- 1.1.3 (Closure of a subset)
+def cl (U : set P) : set P := { x | ∃ y ∈ U, x ≤ y }
+
+lemma subset_cl (U : set P) : U ⊆ cl U := 
+begin
+  intros x hx,
+  use x,
+  apply and.intro hx, refl
+end
+
+lemma mem_cl_of_below {U : set P} {x : P} (hx : x ∈ cl U) :
+  ∀ y : P, y ≤ x → y ∈ cl U :=
+begin
+  intros y hle,
+  cases hx with w hw,
+  cases hw with hw hlew,
+  use w,
+  apply and.intro hw,
+  apply le_trans hle hlew
+end
+
+-- 1.1.4 (Closed subset)
+class is_closed (U : set P) : Prop :=
+(is_eq_cl : U = cl U)
+
+lemma closed_eq_closure (U : set P) [is_closed U] : U = cl U := is_closed.is_eq_cl
+ 
+lemma empty_eq_cl_empty : (∅ : set P) = cl ∅ :=
+begin
+  ext, unfold cl, simp
+end
+
+instance empty_closed : is_closed (∅ : set P) := 
+{ is_eq_cl := empty_eq_cl_empty} 
+
+lemma closure_nonempty {U : set P} (hnempty : U.nonempty) : (cl U).nonempty :=
+nonempty.mono (subset_cl U) hnempty
+
+-- Lemma 1.1.5 - part 1
+lemma closure_monotonic (U V : set P) (hsub : U ⊆ V) : cl U ⊆ cl V :=
+λ x ⟨u, ⟨hu, hle⟩⟩, ⟨u, ⟨hsub hu, hle⟩⟩
+
+-- Lemma 1.1.5 - part 2
+lemma closure_sub {U V: set P} [is_closed V] (hsub : U ⊆ V) : cl U ⊆ V :=
+begin
+  rw closed_eq_closure V,
+  exact closure_monotonic _ _ hsub,
+end
+
+lemma closure_indempotent (U : set P) : cl U = cl (cl U) :=
+begin
+  apply subset_antisymm,
+  { exact subset_cl _ },
+  { intros x hx,
+    cases hx with w hw,
+    cases hw with hw hlew,
+    apply mem_cl_of_below hw _ hlew }
+end
+
+instance closure_is_closed (U : set P) : is_closed (cl U) := 
+{is_eq_cl := closure_indempotent _ }
+
+lemma mem_cl_singleton_iff_below (x y : P) : y ∈ cl ({x} : set P) ↔ y ≤ x :=
+begin
+  split; intro h,
+  { cases h with w hw,
+    cases hw with hw hle,
+    rw mem_singleton_iff at hw,
+    rw hw at hle, exact hle },
+  { use x, 
+    refine and.intro _ h, 
+    exact mem_singleton x }
+end
+
+-- Lemma 1.1.6
+lemma closure_union_eq_union_closure (ι : Sort*) (s : ι → set P) : cl (⋃ i, s i) = ⋃ i, cl (s i) :=
+begin
+  ext, split,
+  { rintro ⟨y, ⟨hin, hle⟩⟩,
+    rw mem_Union at *, 
+    cases hin with i hin,
+    use i, use y,
+    exact and.intro hin hle },
+  { rw mem_Union, 
+    rintro ⟨i, ⟨y, ⟨hin, hle⟩⟩⟩,
+    use y, refine and.intro _ hle,
+    rw mem_Union,
+    exact ⟨i, hin⟩ }
+end 
+
+
+variable (U : set P)
+
+-- 1.1.7 (Maximal element)
+def maximal (x : P) : Prop := (x ∈ U) ∧ ∀ {u}, u ∈ U → ¬(x ⋖ u)
+def Max : set P := { x | maximal U x }
+
+def Max_subset : Max U ⊆ U := 
+begin
+  intros x hx,
+  exact hx.left
+end
+
+-- Sometimes during the proof, we will need some hypothesis on U
+-- That hopefully will no cause any trouble in practice
+instance decidable_subset [fintype U] (x : P) : decidable (x ∈ U) := 
+set.decidable_mem_of_fintype _ _
+
+instance decidable_maximal [fintype U] (x : P) : decidable (maximal U x) := and.decidable
+
+
+-- In this namespace, some auxialiary lemmas to prove Lemma 1.1.8
+namespace maximal
+
+lemma MaxU_sub_U : Max U ⊆ U := λ _ h, h.left
+
+variable [is_closed U]
+
+lemma MaxU_sub_cl_U : cl (Max U) ⊆ U := closure_sub (MaxU_sub_U U)
+
+noncomputable
+def next (xn : P) (hxn : xn ∈ U) : { x // x ∈ U ∧ (maximal U x ∧ xn = x ∨ xn ⋖ x)} :=
+begin
+  by_cases maximal U xn,
+  { use xn, 
+    apply and.intro hxn, left, 
+    apply and.intro h (eq.refl _) },
+  { unfold maximal at h, simp at h,
+    have w := classical.indefinite_description _ (h hxn),
+    -- cases  with w hw,
+    use w.val,
+    apply and.intro w.prop.left, right,
+    exact w.prop.right }
+end
+
+noncomputable
+def seq {x : P} (hx : x ∈ U) : ℕ → { w // w ∈ U } 
+| 0       := ⟨x, hx⟩
+| (n + 1) := let k := next U (seq n).val (seq n).prop in ⟨k.val, k.prop.left⟩
+
+lemma seq_increasing {x : P} (hx : x ∈ U) (hmax : ∀ n, ¬maximal U (seq U hx n)) : 
+  ∀ n, (seq U hx n).val < (seq U hx (n + 1)).val :=
+begin
+  intro n,
+  unfold seq, simp,
+  have prop := (next U (seq U hx n).val (seq U hx n).prop).prop.right,
+  cases prop,
+  { exfalso, apply hmax (n + 1), unfold seq, simp, exact prop.left },
+  { apply covby.lt prop },
+end
+
+lemma seq_increasing_or_eq {x : P} (hx : x ∈ U) : ∀ n, (seq U hx n).val ≤ (seq U hx (n + 1)).val :=
+begin
+  intro n,
+  unfold seq, simp,
+  cases (next U (seq U hx n).val (seq U hx n).prop).prop.right,
+  { exact le_of_eq h.right },
+  { exact covby.le h }
+end
+
+lemma x_le_seq {x : P} (hx : x ∈ U) : ∀ n, x ≤ (seq U hx n).val :=
+begin
+  intro n, induction n with n hn,
+  { refl },
+  { apply le_trans hn, 
+    apply seq_increasing_or_eq }
+end
+
+lemma U_sub_cl_MaxU : U ⊆ cl (Max U) := 
+begin
+  intros x hx, 
+  have hn : ∃ n, maximal U (seq U hx n) := 
+  begin
+    by_contra' h,
+    exact finite.no_infinite_increasing_seq _ (seq_increasing U hx h)
+  end,
+  cases hn with n hn,
+  use seq U hx n,
+  apply and.intro hn,
+  apply x_le_seq,
+end
+
+end maximal
+
+--Lemma 1.1.8
+lemma U_eq_cl_MaxU [is_closed U] : U = cl (Max U) :=
+subset_antisymm (maximal.U_sub_cl_MaxU U) (maximal.MaxU_sub_cl_U U)
+
+
+-- we define the sets of normal elements, the one that are at the bottom of the Hasse diagram
+def is_normal (x : P) : Prop := ∀ y : P, is_empty (y ⟶ x)  
+
+lemma is_normal_not_covered (x : P) (norm : is_normal x) : ∀ y : P, ¬ x ⋖ y :=
+λ y h, (norm y).false h
+
+open quiver
+
+-- A path is maximal iff its codomain is normal
+def is_maximal {x y : P} (p : path x y) : Prop := is_normal y
+
+structure maximal_path (x : P) :=
+(cod : P)
+(normal_cod : is_normal cod)
+(path : path x cod)
+
+lemma is_maximal_concat {x y z : P} (le : x ⟶ y) (p : path y z) (p_max : is_maximal p) : 
+  is_maximal (path.comp (hom.to_path le) p) := p_max
+
+def maximal_path_of_le {x y : P} (le : y ⟶ x) (p : maximal_path x) : maximal_path y :=
+{ cod := p.cod,
+  normal_cod := p.normal_cod,
+  path := path.comp (hom.to_path le) p.path }
+
+def maximal_path_concat {x y : P} (p : path y x) (q : maximal_path x) : maximal_path y :=
+{ cod := q.cod,
+  normal_cod := q.normal_cod,
+  path := path.comp p q.path }
+
+-- An element is graded if all maximal path have same length
+-- To avoid proving there exists a maximal path, we provide one in the class strucutre
+-- In pratice, it should pose no problem, as when we prove all path have the same lenght,
+-- We will most likely exhibit one 
+structure is_graded (x : P) := 
+(dim : ℕ)
+(max_path : maximal_path x)
+(same_length : ∀ p : maximal_path x, dim = path.length p.path)
+
+lemma dim_max_path (x : P) (grad_x : is_graded x) : 
+  grad_x.dim = path.length grad_x.max_path.path := grad_x.same_length grad_x.max_path
+
+variable (P)
+
+-- 1.1.9 (Graded poset)
+class graded :=
+(all_graded : ∀ x : P, is_graded x) 
+
+variables {P} [graded P]
+
+def same_length (x : P) (p q : maximal_path x) : p.path.length = q.path.length :=
+begin
+  rw [←is_graded.same_length, ←is_graded.same_length],
+  exact graded.all_graded x
+end
+
+def max_path (x : P) :=  @is_graded.max_path _ _ _ _ (graded.all_graded x)
+-- 1.1.10 (Dimension of an element)
+def dim (x : P) : nat := (max_path x).path.length
+
+
+-- Auxiliary results to prove that if x < y, then we have a path from y to x
+namespace path_of_lt
+variables {x y : P} 
+
+noncomputable
+def next (xn : P) (hpath : ∀ z : P, path z x → ¬z = y) (hlt : xn < y) (p : path xn x) : 
+  { x // xn ⋖ x ∧ x < y } :=
+begin
+  let k := classical.indefinite_description _ (exists_cov_of_lt hlt),
+  use k.val,
+  apply and.intro k.prop.left,
+  by_cases heq : k.val = y,
+  { exfalso,
+    have hcov : xn ⋖ y := 
+    begin
+      rw ←heq, exact k.prop.left
+    end,
+    apply hpath _ (path.comp (path_of_cov hcov) p), 
+    refl },
+    { rw lt_iff_le_and_ne,
+      exact and.intro k.prop.right heq }
+end
+
+-- A cool use of Σ-types
+noncomputable
+def path_seq (hpath : ∀ z : P, path z x → ¬z = y) (hlt : x < y) : 
+  ℕ → Σ (w : P), { p : path w x // w < y }  
+| 0       := ⟨x, ⟨path.nil, hlt⟩⟩
+| (n + 1) := let v := (next (path_seq n).1 hpath (path_seq n).2.prop (path_seq n).2.val) in
+             ⟨v.val, ⟨path.comp (path_of_cov v.prop.left) (path_seq n).2.val, v.prop.right⟩⟩
+
+-- Now we forget some of the structure of the previous sequence 
+noncomputable
+def path_seq_forget (hpath : ∀ z : P, path z x → ¬z = y) (hlt : x < y) : 
+  ℕ → P := λ n, (path_seq hpath hlt n).1
+
+-- Tis sequence is increasing
+lemma path_seq_forget_cov_increasing (hpath : ∀ z : P, path z x → ¬z = y) (hlt : x < y) :
+  ∀ n, path_seq_forget hpath hlt n ⋖ path_seq_forget hpath hlt (n + 1) :=
+begin
+  intro n,
+  unfold path_seq_forget path_seq, simp,
+  exact (next _ hpath (path_seq hpath hlt n).2.prop (path_seq hpath hlt n).2.val).prop.left
+end
+
+lemma path_seq_forget_increasing (hpath : ∀ z : P, path z x → ¬z = y) (hlt : x < y) :
+  ∀ n, path_seq_forget hpath hlt n < path_seq_forget hpath hlt (n + 1) :=
+begin
+  intro n,
+  apply covby.lt,
+  apply path_seq_forget_cov_increasing
+end
+
+def path_of_lt {x y : P} (hlt : x < y) : ∃ (z : P) (p : path z x), z = y :=
+begin
+  by_contra' h,
+  apply finite.no_infinite_increasing_seq (path_of_lt.path_seq_forget h hlt),
+  apply path_of_lt.path_seq_forget_increasing
+end
+
+end path_of_lt
+
+-- This is the result,
+-- Highly nonconstructible
+noncomputable
+def path_of_lt {x y : P} (hlt : x < y) : path y x :=
+begin
+  have p := classical.indefinite_description _ (path_of_lt.path_of_lt hlt),
+  simp at p,
+  have q := classical.indefinite_description _ p.prop,
+  rw ←q.prop,
+  exact q.val
+end
+
+--Lemma 1.1.12
+lemma dim_monotonic (x y : P) (hle : x ≤ y) : dim x ≤ dim y :=
+begin
+  cases lt_or_eq_of_le hle,
+  { have p := path_of_lt h, 
+    unfold dim,
+    have hh : (maximal_path_concat p (max_path x)).path.length = (max_path y).path.length := 
+      same_length y _ _,
+    rw ←hh, erw path.length_comp, simp },
+  { rw h }
+end
+
+variables (U) 
+
+-- 1.1.13 (Grading of a subset)
+def grading (n : ℕ) : set P := { x ∈ U | dim x = n }
+
+@[simp] lemma mem_grading_dim (x : P) (hx : x ∈ U) : x ∈ grading U (dim x) :=
+begin
+  unfold grading, simp [hx],
+end
+
+lemma eq_union_grading : U = ⋃ n, grading U n :=
+begin
+  apply subset_antisymm; intros x hx; simp at *,
+  { use dim x, simp [hx]},
+  { cases hx with _ h, erw mem_set_of_eq at h,
+    exact h.left }
+end
+
+variable [is_closed U]
+
+
+-- Various auxiliary results about dimension
+namespace dim
+
+-- This won't be the final dim_set
+-- We only use it to prove it's bounded
+def dim_set : set ℕ := { n | ∃ x ∈ U, n = dim x }
+
+@[simp] lemma dim_set_of_is_empty (hempty : U = ∅) : dim_set U = ∅ :=
+begin
+  rw ←subset_empty_iff,
+  intros x hx,
+  cases hx with _ hx, cases hx with hx, 
+  rw hempty at hx,
+  rw mem_empty_iff_false at hx,
+  exfalso, exact hx
+end
+
+@[simp] lemma dim_set_empty : dim_set (∅ : set P) = ∅ :=
+begin
+  apply dim_set_of_is_empty, refl,
+end
+
+noncomputable
+def pick {n : ℕ} (hn : n ∈ dim_set U) : {x : P // x ∈ U ∧ n = dim x} :=
+begin
+  convert (classical.indefinite_description _ hn), 
+  simp,
+end
+
+noncomputable
+def undounded_pick_greater (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) (n : ℕ) : 
+  {x : P // n < dim x } :=
+begin
+  have k := classical.indefinite_description _ (h n),
+  have pt := pick U k.prop.left,
+  refine ⟨pt.val, _⟩,
+  erw ←pt.prop.right,
+  exact k.prop.right,
+end
+
+variables {U} (x0 : P) (hx0 : x0 ∈ U)
+
+noncomputable
+def next (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) (xn : P) := undounded_pick_greater U h (dim xn)
+
+noncomputable
+def dim_seq (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) : ℕ → P 
+| 0       := x0
+| (n + 1) := next h (dim_seq n)
+
+lemma dim_seq_lt_succ (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) :
+  ∀ n, dim (dim_seq x0 h n) < dim (dim_seq x0 h (n + 1)) :=
+begin
+  intro n,
+  unfold dim_seq next,
+  exact (undounded_pick_greater U h (dim (dim_seq x0 h n))).prop,
+end
+
+lemma dim_seq_lt_pred (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) :
+  ∀ n m, n < m → dim (dim_seq x0 h n) < dim (dim_seq x0 h m) :=
+begin
+  intros n m,
+  induction m with m hm,
+  { intro hinf, exfalso, exact not_lt_bot hinf },
+  intro hlt,
+  rw nat.lt_succ_iff_lt_or_eq at hlt,
+  cases hlt,
+    { specialize hm hlt, 
+      apply has_lt.lt.trans hm,
+      apply dim_seq_lt_succ },
+    {rw hlt, apply dim_seq_lt_succ }
+end
+
+lemma dim_congr_contra (x y : P) : dim x < dim y → x ≠ y :=
+begin
+  by_contra' h,
+  have k := h.left,
+  rw h.right at k,
+  apply lt_irrefl _ k,
+end
+
+lemma dim_seq_neq_preds (h : ∀ (N : ℕ), ∃ (n : ℕ), n ∈ dim_set U ∧ N < n) :
+  ∀ n m, n < m → dim_seq x0 h n ≠ dim_seq x0 h m :=
+begin
+  intros n m hlt,
+  apply dim_congr_contra,
+  apply dim_seq_lt_pred,
+  exact hlt
+end
+
+-- As long as we can provide a point in P, the dimension of every point is bounded
+lemma bounded_dim_set' (x : P) (U : set P) [is_closed U] : ∃ N, ∀ n ∈ dim_set U, n ≤ N :=
+begin
+  by_contra' h,
+  apply finite.no_forall_neq_preds (dim_seq x h),
+  apply dim_seq_neq_preds,
+end
+
+-- And if P is empty, we can as well show it is bounded (trivially)
+lemma bounded_dim_set (U : set P) [is_closed U] : ∃ N, ∀ n ∈ dim_set U, n ≤ N :=
+begin
+  by_cases h : is_empty P,
+  { use 0, intros n hn, exfalso, 
+    exact h.false (pick U hn).val }, 
+  { simp at h,
+    apply bounded_dim_set' (classical.choice h) }
+end
+
+-- Do I really need to do this?
+noncomputable
+def dim_set_injective (U : set P) [is_closed U] : 
+  Σ (N : ℕ), { f : dim_set U → fin N // function.injective f } :=
+begin
+  have N := classical.indefinite_description _ (bounded_dim_set U),
+  use N.val + 1,
+  refine ⟨(λ n : dim_set U, {val := n, property := _ }), _ ⟩,
+  apply nat.lt_succ_of_le,
+  apply N.prop, simp,
+  intros m n heq, simp at heq, ext, exact heq,
+end
+
+-- A number such that Dim U is less than it
+noncomputable
+def upper_bound (U : set P) [is_closed U] : { N // ∀ n ∈ dim_set U, n ≤ N } := 
+classical.indefinite_description _ (bounded_dim_set U)  
+
+lemma dim_mem_dim_set (U : set P) [is_closed U] : ∀ x ∈ U, dim x ∈ dim_set U :=
+begin
+  intros x hx,
+  use x,
+  exact and.intro hx (eq.refl _)
+end
+
+-- The two useful constructions from this namespace
+
+-- The upper bound
+noncomputable
+def M (U : set P) [is_closed U] := (dim.upper_bound U).val
+
+-- The fact that it's an upper bound
+lemma M_prop {U : set P} [is_closed U] : ∀ x ∈ U, dim x ≤ M U :=
+begin
+  intros x hx,
+  exact (upper_bound U).prop (dim x) (dim_mem_dim_set U x hx)
+end
+
+end dim
+
+noncomputable
+def dim_set : finset ℕ := 
+finset.filter (λ n, ∃ x ∈ U, n = dim x) (finset.Icc 0 (dim.M U))
+
+lemma dim_set_complete : ∀ x ∈ U, dim x ∈ dim_set U :=
+begin
+  intros x hx,
+  erw finset.mem_filter,
+  split,
+  { rw finset.mem_Icc, simp, apply dim.M_prop _ hx },
+  { use x, use hx }
+end
+
+@[simp] lemma dim_set_empty : dim_set (∅ : set P) = ∅ :=
+begin
+  unfold dim_set, simp,
+end
+
+@[simp] lemma dim_set_of_is_empty (hempty : U = ∅) : dim_set U = ∅ :=
+begin
+  unfold dim_set, ext, simp,
+  intros hle x hx,
+  rw [hempty, set.mem_empty_iff_false] at hx,
+  exfalso,
+  exact hx
+end
+
+-- 1.1.14 (Dimension of a subset)
+noncomputable
+def Dim [is_closed U] : with_bot ℕ := finset.max (dim_set U)
+
+lemma Dim_empty : Dim (∅ : set P) = ⊥ := by { unfold Dim, simp }
+
+lemma dim_set_nonempty_of_U_nonempty {U : set P} [is_closed U] (hnempty : U.nonempty) : 
+  (dim_set U).nonempty :=
+begin
+  cases hnempty with w hw,
+  use dim w,
+  apply dim_set_complete U w hw,
+end
+
+-- To work directly in ℕ with non empty sets
+noncomputable
+def Dim' [is_closed U] (hnempty : U.nonempty) : ℕ := 
+finset.max' (dim_set U) (dim_set_nonempty_of_U_nonempty hnempty)
+
+lemma Dim_eq_Dim' [is_closed U] (hnempty : U.nonempty) : some (Dim' U hnempty) = Dim U :=
+finset.coe_max' _
+
+-- 1.1.15
+lemma Dim'_cl_singleton (x : P) : 
+  Dim' (cl ({x} : set P)) (closure_nonempty (set.singleton_nonempty x)) = dim x :=
+begin
+  unfold Dim', 
+  apply le_antisymm,
+  { simp, intros y hy,
+    erw finset.mem_filter at hy, simp at hy,
+    cases hy.right with w hw,
+    rw hw.right,
+    apply dim_monotonic,
+    rw ←mem_cl_singleton_iff_below,
+    exact hw.left },
+  { apply finset.le_max', 
+    apply dim_set_complete,
+    apply subset_cl,
+    exact mem_singleton x }
+end
+
+lemma Dim_cl_singleton (x : P) : Dim (cl ({x} : set P)) = some (dim x) :=
+by { rw [←Dim_eq_Dim', Dim'_cl_singleton] }
+
+-- 1.1.16 (Codimension of an element)
+noncomputable
+def coDim {U : set P} [is_closed U] {x : P} (hx : x ∈ U) : ℤ := 
+  Dim' U (set.nonempty_of_mem hx) - dim x
+
+def coDim_positive {U : set P} [is_closed U] {x : P} (hx : x ∈ U) : 0 ≤ coDim hx :=
+begin
+  unfold coDim,
+  rw [sub_nonneg, nat.cast_le],
+  apply finset.le_max' _ _ (dim_set_complete _ _ hx)
+end
+
+-- 1.1.17 (Pure subset).
+def pure (U : set P) [is_closed U] := 
+  ∀ x ∈ Max U, dim x = Dim' U (set.nonempty_of_mem (Max_subset U H))
+
+end ogpos
